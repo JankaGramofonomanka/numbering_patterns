468d2943c6b8c5bae58ce728737a1ac886f202cb
JankaGramofonomanka <tsrn666#gmail.com>
2020-03-16 21:00:21 +0100

    Add the test_all.py file
    Added a file that tests all of the classes.

---
+++ numbering_patterns/tests/test_all.py
@@ -0,0 +1,13 @@
+import unittest
+
+from test_lf_init import TestInit
+from test_lf_magic_methods import TestMagicMethods
+from test_lf_modifiers import TestModifiers
+from test_lf_other import TestOther
+
+from test_ntr_sequence import TestNTRSequence
+from test_cv_numbering import TestCVNP
+
+if __name__ == '__main__':
+
+    unittest.main()
